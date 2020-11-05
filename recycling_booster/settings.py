234277560fc3204f2722cb9e176fc554b7f694e9
xicarik <anonymous@anonymous.com>
2020-11-05 22:39:27 +0300

    fix bugs

--- recycling_booster/settings.py
+++ recycling_booster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '67xie0hq%f!e)xb&jg6g$c9yxvx^1klsgc0lb$e%gdeern=poh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['recyclingbooster.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tranquil-scrubland-01368.herokuapp.com', '127.0.0.1']
 
 
 MEDIA_URL = '/media/'
